d4063093de022f95a235733e44d2f190fbcee824
hysteriaskiwo <cerynna@gmail.com>
2017-09-18 14:58:33 +0200

    EYE OF SAURON

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Page Description">
+    <meta name="author" content="hysterias">
+    <title>Page Title</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+</head>
+<body>
+<header id="header" class="fond_neutre">
+
+</header>
+
+<section>
+
+    <div class="container">
+
+        <!-- Row 1-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-9 noir"></div>
+        </div>
+
+        <!-- Row 2-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-3 orange"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 col-xs-offset-3 noir"></div>
+        </div>
+
+        <!-- Row 3-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-2 orange"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 noir2"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 col-xs-offset-2 noir"></div>
+        </div>
+
+        <!-- Row 4-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 noir2"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 5-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-1 orange"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 noir2"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 col-xs-offset-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 6-->
+        <div class="row">
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-1 orange"></div>
+            <div class="col-xs-1 jaune"></div>
+            <div class="col-xs-1 orange"></div>
+            <div class="col-xs-1 col-xs-offset-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 7-->
+        <div class="row">
+            <div class="col-xs-1 noir col-xs-offset-1"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 col-xs-offset-3 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 8-->
+        <div class="row">
+            <div class="col-xs-1 col-xs-offset-2 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 9-->
+        <div class="row">
+            <div class="col-xs-1 col-xs-offset-3 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 10-->
+        <div class="row">
+            <div class="col-xs-1 col-xs-offset-4 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+
+        <!-- Row 11-->
+        <div class="row">
+            <div class="col-xs-1 col-xs-offset-4 noir"></div>
+            <div class="col-xs-1 noir"></div>
+            <div class="col-xs-1 noir"></div>
+        </div>
+    </div>
+
+</section>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
